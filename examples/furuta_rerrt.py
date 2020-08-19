22e084bdf86de4a6d2fdd23b5b123edb6a6200f9
phil <filipjasper@gmail.com>
2020-08-19 12:42:12 -0400

    Furuta example functional, will add diff distanceMetric

---
+++ examples/furuta_rerrt.py
@@ -0,0 +1,114 @@
+"""
+RERRT Example
+"""
+
+# python libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# custom classes
+from trees.rrt import RRT
+from trees.rerrt import RERRT
+from utils.shapes import Rectangle, Ellipse
+from utils.collision import CollisionDetection
+from systems.primitives import Input
+from systems.examples import Furuta
+from visuals.helper import pickRandomColor
+from visuals.plotting import (Scene, drawScene, drawTree, drawReachable,
+                              drawEllipsoids, drawEllipsoidTree)
+
+
+# Initialize start, goal, bounds on area
+start = [12.5, 12.5]
+goal = [0, 0]
+region = Rectangle([-5, -5], 20, 20)
+
+# start_state used for forward expansion as start
+# goal_state used for backward expansion as start
+# currently RRT does not require both starting state and ending state to belong to final tree
+# one must be the root of tree and returns solution closest to other desired state
+start_state = np.array(start + [-np.pi*2/3, 5, 0]).reshape(5, 1)
+goal_state = np.array(goal + [-np.pi*2/3, 5, 0])
+
+# initialize obstacles
+# no obstacles currently
+obstacles = []
+
+# Scene describes the start, goal, obstacles, mostly for plotting
+scene = Scene(start, goal, region, obstacles)
+
+# could automate system config
+sys_opts = {
+    'dt': 0.005,
+    'nx': 4,
+    'nu': 2,
+    'nw': 2
+    }
+sys = Furuta(sys_opts)
+
+#   'dt'   :float:  timestep
+#   'nx'   :int:    dim of state
+#   'nu'   :int:    dim of input
+#   'nw'   :int:    dim of uncertainty
+
+# named input_ to avoid conflict with python keyword input
+input_ = Input(dim=sys_opts['nu'], type_='deterministic')
+# no motor at second joint, making underactuated
+input_.setLimits(np.array([10, 0]))
+input_.determinePossibleActions(resolutions=np.array([3, 1]))
+
+#   setLimits          args     :nparray: (dim(input),)         max magnitude of each input
+#   setType            args     :'random'/'deterministic':      input sampling type, often abbrv. input type 
+#   setNumSamples      args     :int:                           if input type random, num random samples
+#   determineActions   args     :nparray: (dim(input),)         if input type deterministic, calculates deterministic actions  
+
+col = CollisionDetection()
+collision_function = col.selectCollisionChecker('erHalfMtxPts')
+
+
+# options to configure RERRT initialization and expansion
+run_options = {
+    'min_dist':         1e-1,                             # :float:                       min dist to goal
+    'max_iter':         50,                            # :int:                         iterations
+    'direction':        'backward',                     # :'backward'/'forward':        determine tree growth direction
+    'track_children':   True,                           # :bool:                        keep record of children of node
+    'extend_by':        20,                             # :int:                         num timesteps to simulate in steer function with each extension
+    'goal_sample_rate': 0.20,                           # :float:                       goal sample freq. (out of 1)
+    'sample_dim':       2,                              # :int:                         Determine how many dimensions to sample in, e.g. 2 for 2D
+    'distanceMetric':   None,                           # :function:                    By default 2D euclidean norm, but can pass in alternatives
+    'D':                0.00*np.eye(sys_opts['nw']),    # :nparray: (nw x nw)           ellipse describing uncertainty
+    'E0':               0.10*np.eye(sys_opts['nx']),    # :nparray: (nx x nx)           initial state uncertainty
+    'Q':                np.diag((5, 5, 0, 0)),       # :nparray: (nx x nx)           TVLQR Q
+    'R':                np.eye(sys_opts['nu']),         # :nparray: (nu x nu)           TVLQR R
+    'Ql':               np.eye(sys_opts['nx']),         # :nparray: (nx x nx)           use if robust cost from DIRTREL paper added
+    'Rl':               np.eye(sys_opts['nu']),         # :nparray: (nu x nu)           see above
+    'QlN':              np.eye(sys_opts['nx']),         # :nparray: (nx x nx)           see above
+}
+
+# initialize RERRT
+tree = RERRT(start=start_state,
+             goal=goal_state,
+             system=sys,
+             input_=input_,
+             scene=scene,
+             collision_func=collision_function,
+             opts=run_options)
+
+print('\nTree Expanding...')
+tree.treeExpansion(run_options)
+print('\nPlotting...')
+final_path = tree.finalPath()
+# order determines what gets occluded in figure
+drawScene(scene, size=(15, 15))
+# drawScene is called first as it creates the figure then shows region+obstacles
+#drawReachable(tree.node_list, fraction=1.00)
+# fractional plotting is used as dataset becomes huge
+drawTree(tree, color='blue')
+#drawPath(final_path, color='red')
+# hlfmtxpts drawing currently is not optimized
+#drawEllipsoids(final_path, hlfmtxpts=False, fraction=1.00)
+drawEllipsoidTree(tree, run_options)
+print('Finished\n')
+plt.show()
+
+
